14b951c24fdefa28dcf8659f556e18cfa825456b
Kevin <kdoherty@mit.edu>
2018-07-02 13:57:39 -0400

    Demo working on GPU

--- demo/live.py
+++ demo/live.py
@@ -12,20 +12,33 @@ parser.add_argument('--weights', default='weights/ssd_300_VOC0712.pth',
 parser.add_argument('--cuda', default=False, type=bool,
                     help='Use cuda in live demo')
 args = parser.parse_args()
+print(args)
+
+if torch.cuda.is_available():
+    if args.cuda:
+        torch.set_default_tensor_type('torch.cuda.FloatTensor')
+    if not args.cuda:
+        print("WARNING: It looks like you have a CUDA device, but aren't using \
+              CUDA.  Run with --cuda for optimal eval speed.")
+        torch.set_default_tensor_type('torch.FloatTensor')
+else:
+    torch.set_default_tensor_type('torch.FloatTensor')
 
 COLORS = [(255, 0, 0), (0, 255, 0), (0, 0, 255)]
 FONT = cv2.FONT_HERSHEY_SIMPLEX
 
 
-def cv2_demo(net, transform):
+def cv2_demo(net, transform, cuda):
     def predict(frame):
         height, width = frame.shape[:2]
         x = torch.from_numpy(transform(frame)[0]).permute(2, 0, 1)
         x = Variable(x.unsqueeze(0))
+        if cuda:
+            x = x.cuda()
         y = net(x)  # forward pass
         detections = y.data
         # scale each detection back up to the image
-        scale = torch.Tensor([width, height, width, height])
+        scale = torch.cuda.FloatTensor([width, height, width, height])
         for i in range(detections.size(1)):
             j = 0
             while detections[0, i, j, 0] >= 0.6:
@@ -76,10 +89,12 @@ if __name__ == '__main__':
 
     net = build_ssd('test', 300, 21)    # initialize SSD
     net.load_state_dict(torch.load(args.weights))
+    if args.cuda:
+        net = net.cuda()
     transform = BaseTransform(net.size, (104/256.0, 117/256.0, 123/256.0))
 
     fps = FPS().start()
-    cv2_demo(net.eval(), transform)
+    cv2_demo(net.eval(), transform, args.cuda)
     # stop the timer and display FPS information
     fps.stop()
 
